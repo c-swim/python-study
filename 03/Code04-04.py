99f9f18f2a5e21d8a20b154a683bd026f606112e
c-swim <cswim914@gmail.com>
2025-05-12 17:17:13 +0900

    03

---
+++ 03/Code04-04.py
@@ -0,0 +1,12 @@
+#Code04-04.py
+a=100
+result=0
+i=0
+
+for i in range(1,5):
+  result = a<<i
+  print("%d << %d = %d" %(a,i,result))
+
+for i in range(1,5):
+  result = a>>i
+  print("%d >> %d = %d" %(a,i,result))
