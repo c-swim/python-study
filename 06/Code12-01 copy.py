12a97527a9ed72e81e34a63ca83a1521702096ea
c-swim <cswim914@gmail.com>
2025-05-29 09:16:53 +0900

    06-1

---
+++ 06/Code12-01 copy.py
@@ -0,0 +1,17 @@
+class Car :
+    color = ""
+    speed = 0
+    
+    def upSpeed(self, value) :
+        self.speed += value
+    def downSpeed(self, value) :
+        self.speed -= value
+
+myCar1 = Car()
+myCar1.color = "빨강"
+myCar1.speed = 0
+
+myCar2 = Car()
+myCar2.color = "파랑"
+myCar2.speed = 0
+
